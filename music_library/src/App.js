6a4e3c26bcc739f1b83cc756973335861617305b
Susan Heller <*jefa8@gmail.com>
2022-10-28 10:19:55 -0400

    useState and useEffect

--- music_library/src/App.js
+++ music_library/src/App.js
@@ -1,7 +1,9 @@
+// IMPORTS
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 
+// MAIN REACT COMPONENT
 function App(){
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
@@ -29,6 +31,16 @@ function App(){
         </div>
     )
 }
+// Lines 8-10 uses "useState" hook to set initial values 
+//  these values (state) is preserved for re-renders of the page.
+//  "useState" returns a pair: the current state value and a function that lets you update the state (ex= [search, setSearch])
+//  Note that if updated it does not merge the old and new states.  State is replaced.  If you need to add to current state 
+//  you will need to include both current state and new state values (it is not a append, it is a replace).
+// Lines 12-24 uses "useEffect" hook to set-up a fetch
+// Line 24 contains a dependency that only allow to search to execute if the value of search is updated.
 
+
+
+// EXPORT, DUH
 export default App
 
