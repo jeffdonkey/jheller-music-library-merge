77afb1d08f09a54d042d4f88e34d71c372460507
Susan Heller <*jefa8@gmail.com>
2022-10-29 12:16:17 -0400

    done with bonus steps 7 and 8

--- music_library/src/App.js
+++ music_library/src/App.js
@@ -9,37 +9,37 @@ function App(){
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
 
+    const API_URL = 'https://itunes.apple.com/search?term='
+
     useEffect(() => {
-        const fetchData = async () => {
-            document.title = `${search} Music`
-            const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-            const resData = await response.json()
-            if (resData.results.lenght > 0) {
-                setData(resData.results)
-            } else {
-                setMessage('Not Found')
+        if(search) {
+            const fetchData= async () => {
+                document.title = `${search} music`
+                const response = await fetch (API_URL + search)
+                const resData = await response.json()
+                if (resData.results.length > 0) {
+                    return setData(resData.results)
+                } else {
+                    return setMessage('Not Found')
+                }
             }
         }
         fetchData()
     }, [search])
 
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        setSearch(term)
+    }
+
     return (
         <div>
-            <SearchBar />
+            <SearchBar handleSearch = {handleSearch}/>
             {message}
             <Gallery />
         </div>
     )
 }
-// Lines 8-10 uses "useState" hook to set initial values 
-//  these values (state) is preserved for re-renders of the page.
-//  "useState" returns a pair: the current state value and a function that lets you update the state (ex= [search, setSearch])
-//  Note that if updated it does not merge the old and new states.  State is replaced.  If you need to add to current state 
-//  you will need to include both current state and new state values (it is not a append, it is a replace).
-// Lines 12-24 uses "useEffect" hook to set-up a fetch
-// Line 24 contains a dependency that only allow to search to execute if the value of search is updated.
-
-
 
 // EXPORT, DUH
 export default App
--- music_library/src/components/Gallery.js
+++ music_library/src/components/Gallery.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 import GalleryItem from './GalleryItem'
 
 function Gallery(props) {
+
+  const display = props.data.map ((item, index) => {
+    return (
+      <GalleryItem item={item} key={index} />
+    )
+  })
+
   return (
     <div>
-        <GalleryItem />
+      {display}
     </div>
   )
 }
+  
+  
 
 export default Gallery
